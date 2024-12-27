58b68583c4e0201ffe2feb7543e95b259672d2d7
OkarRojas <orojasb@unal.edu.co>
2024-12-27 18:21:06 -0500

    Plan
    Añadido metodos

--- paymentManager/src/main/java/gestorAplicacion/transactions/Transaction.java
+++ paymentManager/src/main/java/gestorAplicacion/transactions/Transaction.java
@@ -4,14 +4,16 @@ import java.util.List;
 
 import gestorAplicacion.WithId;
 import gestorAplicacion.customers.Admin;
-import gestorAplicacion.plan.Suscription;
+import gestorAplicacion.plan.Subscription;
 
 public class Transaction extends WithId {
     private String name;
     private String description;
     private double price;
     private int duration;
-    
+    private Card paymentMethod;
+    private  TransactionStatus status;
+
     public Transaction(String id, String name, String description, double price, int duration, Admin[] admins) {
         super(id);
         this.name = name;
@@ -20,8 +22,16 @@ public class Transaction extends WithId {
         this.duration = duration;
     }
  
-    public static void mostrarSuscripciones(List<Suscription> suscripciones) {
-        for (Suscription suscription : suscripciones) {
+    public Card getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(Card paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public static void mostrarSuscripciones(List<Subscription> suscripciones) {
+            for (Subscription suscription : suscripciones) {
             System.out.println("ID: " + suscription.getId());
             System.out.println("Nombre: " + suscription.getName());
             System.out.println("Descripción: " + suscription.getDescription());
@@ -30,4 +40,13 @@ public class Transaction extends WithId {
             System.out.println("-------------------------");
         }
     }
+
+    public TransactionStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TransactionStatus status) {
+        this.status = status;
+    }
+    
 }
---
+++ paymentManager/src/main/java/gestorAplicacion/transactions/TransactionStatus.java
@@ -0,0 +1,9 @@
+package gestorAplicacion.transactions;
+
+public enum TransactionStatus {
+    ACCEPTED,
+    REJECTED,
+    PENDING,
+    CANCELLED,
+    REVERSED,
+}
